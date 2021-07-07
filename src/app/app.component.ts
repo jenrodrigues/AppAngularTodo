e6e99db80cdb2545f0ac93621b2f13c2361c5cfa
Jenifer Rodrigues <jen.mrodrigues@gmail.com>
2021-07-07 11:43:23 +0100

    Commit inicial da aplicacao em TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AppAngular';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+
+  }
+
+  alteraTexto() {
+    this.title = 'Teste';
+  }
 }
