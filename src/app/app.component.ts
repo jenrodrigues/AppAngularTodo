28886ad32bab219ed30942cae440d3642e596aba
Jenifer Rodrigues <jen.mrodrigues@gmail.com>
2021-07-09 11:02:17 +0100

    Estilizando o form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,10 +20,20 @@ export class AppComponent {
         Validators.required
       ])]
     });
-    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
-    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
 
+    this.load();
+  }
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
@@ -33,15 +43,32 @@ export class AppComponent {
       this.todos.splice(index, 1);
     }
 
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
 
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
 
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    }
+    else {
+      this.todos = [];
+    }
+
+  }
 }
